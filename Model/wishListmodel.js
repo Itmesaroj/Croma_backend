b071f2f10dfe4ab030632b51d8901f3e9aae8cea
itmesaroj <itsmesaroj007@gmail.com>
2024-08-01 23:39:22 +0530

    Resolved merge conflicts

--- Model/wishListmodel.js
+++ Model/wishListmodel.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const wishListSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  image: { type: String },
-  title: { type: String },
-  aprice: { type: Number },
-  pprice: { type: Number },
-  savemoney: { type: Number },
-  features: [String],
-  rating: { type: Number },
-  brand: { type: String }
+    image: { type: String, required: true },
+    title: { type: String, required: true },
+    aprice: { type: Number, required: true },
+    pprice: { type: Number, required: true },
+    savemoney: { type: String, required: true },
+    features: { type: [String], required: true }  ,
+    rating:{type:Number,required:true},
+    brand:{type:String,required:true},
+   email:{type:String,required:true}
 });
 
-module.exports = mongoose.model('wishlists', wishListSchema); // Ensure the model name is correct
+module.exports = mongoose.model('wishlists', wishListSchema); 
